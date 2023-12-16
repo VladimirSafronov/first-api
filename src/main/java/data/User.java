359f3d461e3f78b5bb464e15597d1eb6cfcbbaf1
Vladimir Safronov <safron5000@mail.ru>
2023-12-16 23:52:54 +0300

    Create tests
     - add DataProvider
     - add Specification

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -3,7 +3,7 @@ package data;
 public class User {
 
   private int id;
-  private String email;
+  protected String email;
   private String first_name;
   private String last_name;
   private String avatar;
